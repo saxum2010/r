d03edbdca31e756ba96d49bd8418302d271bfbb6
saxum2010 <saxum2010@gmail.com>
2019-08-22 11:59:51 +0300

    lesson 2

--- src/app.js
+++ src/app.js
@@ -1,14 +1,84 @@
 import React from 'react';
-import CounterClass from './counters/class.js';
-import CounterFunction from './counters/function.js';
+import AppMinMax from './hw/5-norm.js';
 
-export default function(){
-    return (
-        <div>
-            <h2>Counter as class</h2>
-            <CounterClass/>
-            <h2>Counter as function</h2>
-            <CounterFunction/>
-        </div>
-    );
+export default class extends React.Component{
+    state = {
+        products: [
+            {
+                id: 100,
+                title: 'Ipnone 200',
+                price: 12000,
+                rest: 10,
+                current: 1
+            },
+            {
+                id: 101,
+                title: 'Samsung AAZ8',
+                price: 22000,
+                rest: 5,
+                current: 1
+            },
+            {
+                id: 103,
+                title: 'Nokia 3310',
+                price: 5000,
+                rest: 2,
+                current: 1
+            },
+            {
+                id: 105,
+                title: 'Huawei ZZ',
+                price: 15000,
+                rest: 8,
+                current: 1
+            }
+        ]
+    }
+
+    changeCnt(i, cnt){
+        // по смысле this.state.products[i].current = cnt;
+
+        let newProducts = [...this.state.products];
+        let newProduct = {...newProducts[i]};
+        newProduct.current = cnt;
+        newProducts[i] = newProduct;
+        this.setState({products: newProducts});
+    }
+
+    render(){
+        let productsRows = this.state.products.map((product, i) => {
+            return (
+                <tr key={product.id}>
+                    <td>{product.title}</td>
+                    <td>{product.price}</td>
+                    <td>
+                        <AppMinMax min={1} 
+                                   max={product.rest} 
+                                   cnt={product.current} 
+                                   onChange={(cnt) => this.changeCnt(i, cnt)}
+                        />
+                    </td>
+                    <td>{product.price * product.current}</td>
+                </tr>
+            );
+        });
+
+        return (
+            <div>
+                <h2>Cart</h2>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Title</td>
+                            <td>Price</td>
+                            <td>Count</td>
+                            <td>Total</td>
+                        </tr>
+                        {productsRows}
+                    </tbody> 
+                </table>
+                
+            </div>
+        );
+    }
 }
--- src/main.js
+++ src/main.js
@@ -1,26 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './app.js';
+import AppOld from './appDerived.js';
 
 ReactDom.render(<App/>, document.querySelector('#app'));
-
-/*
-    let hr = React.createElement('hr');
-    let div = React.createElement('div', {className: 'test'}, [hr]);
-
-    <div className="test">
-        <span>Hello!</span>    
-        <hr/>
-    </div>
-*/
-
-/*
-    class -> className
-    
-    events
-        oninput -> onChange
-        onchange -> can`t
-        ondblclick -> onDoubleClick
-
-    some={x}
-*/
